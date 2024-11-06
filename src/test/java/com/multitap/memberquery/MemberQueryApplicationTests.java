63e49a7bef2a0667234d6370999b6148ebb65373
kang-subin <tnbin98@gmail.com>
2024-11-06 10:24:10 +0900

    Initial commit

---
+++ src/test/java/com/multitap/memberquery/MemberQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multitap.memberquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
